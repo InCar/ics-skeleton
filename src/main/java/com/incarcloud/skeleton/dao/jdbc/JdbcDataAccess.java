de390ce9cec6ad64b4acc36ae6ce322eff53a96f
baichangda <471267877@qq.com>
2019-01-03 14:26:53 +0800

    数据库连接池

--- src/main/java/com/incarcloud/skeleton/dao/jdbc/JdbcDataAccess.java
+++ src/main/java/com/incarcloud/skeleton/dao/jdbc/JdbcDataAccess.java
@@ -9,12 +9,15 @@ import com.incarcloud.skeleton.config.JdbcConfig;
 import com.incarcloud.skeleton.context.Context;
 import com.incarcloud.skeleton.context.Initializable;
 import com.incarcloud.skeleton.dao.DataAccess;
+import com.incarcloud.skeleton.dao.jdbc.sql.SqlListResult;
+import com.incarcloud.skeleton.dao.jdbc.sql.SqlUtil;
 import com.incarcloud.skeleton.exception.BaseRuntimeException;
 import com.incarcloud.skeleton.page.Page;
 import com.incarcloud.skeleton.page.PageResult;
 
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.TimeUnit;
@@ -35,7 +38,7 @@ public class JdbcDataAccess implements DataAccess<Connection>,Initializable{
     public <R>R doInConnection(Function<Connection,R> function) {
         JdbcConfig jdbcConfig= context.getConfig().getJdbcConfig();
         try {
-            Connection connection= pool.pollLast(jdbcConfig.getMaxWaitSeconds(), TimeUnit.SECONDS);
+            Connection connection= pool.pollFirst(jdbcConfig.getMaxWaitSeconds(), TimeUnit.SECONDS);
             if(connection==null){
                 String msg="Wait Jdbc Pool More Than ["+jdbcConfig.getMaxWaitSeconds()+"],No Available Connection";
                 throw BaseRuntimeException.getException(msg);
@@ -44,7 +47,9 @@ public class JdbcDataAccess implements DataAccess<Connection>,Initializable{
                     if(!connection.isValid(0)){
                         try{
                             connection.close();
-                        }finally {
+                        }catch (SQLException e){
+                            //do nothing
+                        }finally{
                             connection=getConnection();
                         }
                     }
@@ -90,12 +95,15 @@ public class JdbcDataAccess implements DataAccess<Connection>,Initializable{
     }
 
     public <T>List<T> list(String sql, RowHandler<T> rowHandler, Object ... params){
+        SqlListResult sqlListResult= SqlUtil.replaceNull(sql, Arrays.asList(params));
+        String newSql=sqlListResult.getSql();
+        List<Object> newParamList= sqlListResult.getParamList();
         return doInConnection(connection -> {
-            try(PreparedStatement ps=connection.prepareStatement(sql)){
+            try(PreparedStatement ps=connection.prepareStatement(newSql)){
                 List<T> resultList=new ArrayList<>();
-                if(params!=null&&params.length>0){
-                    for(int i=1;i<=params.length;i++){
-                        ps.setObject(i,params[i-1]);
+                if(newParamList!=null&&newParamList.size()>0){
+                    for(int i=1;i<=newParamList.size();i++){
+                        ps.setObject(i,newParamList.get(i-1));
                     }
                 }
                 long t1=System.currentTimeMillis();
@@ -103,9 +111,9 @@ public class JdbcDataAccess implements DataAccess<Connection>,Initializable{
                 long t2=System.currentTimeMillis();
                 long diff=t2-t1;
                 if(diff>WARNING_SQL_TIME_MILLS){
-                    Config.GLOBAL_LOGGER.warning("Sql["+sql+"] Take "+diff);
+                    Config.GLOBAL_LOGGER.warning("Sql["+newSql+"] Take "+diff);
                 }else{
-                    Config.GLOBAL_LOGGER.info("Sql["+sql+"] Take "+diff);
+                    Config.GLOBAL_LOGGER.info("Sql["+newSql+"] Take "+diff);
                 }
                 while(rs.next()){
                     T t= rowHandler.apply(rs);
@@ -119,13 +127,16 @@ public class JdbcDataAccess implements DataAccess<Connection>,Initializable{
     }
 
     public <T>PageResult<T> page(String countSql, String sql, RowHandler<T> rowHandler, Page page, Object ... params){
+        SqlListResult countSqlListResult= SqlUtil.replaceNull(countSql, Arrays.asList(params));
+        String newCountSql=countSqlListResult.getSql();
+        List<Object> newCountParamList= countSqlListResult.getParamList();
         return doInConnection(connection -> {
             int count;
             try{
-                try (PreparedStatement countPs = connection.prepareStatement(countSql)) {
-                    if (params != null && params.length > 0) {
-                        for (int i = 1; i <= params.length; i++) {
-                            countPs.setObject(i, params[i]);
+                try (PreparedStatement countPs = connection.prepareStatement(newCountSql)) {
+                    if (newCountParamList != null && newCountParamList.size() > 0) {
+                        for (int i = 1; i <= newCountParamList.size(); i++) {
+                            countPs.setObject(i, newCountParamList.get(i-1));
                         }
                     }
                     long t1=System.currentTimeMillis();
@@ -133,9 +144,9 @@ public class JdbcDataAccess implements DataAccess<Connection>,Initializable{
                     long t2=System.currentTimeMillis();
                     long diff=t2-t1;
                     if(diff>WARNING_SQL_TIME_MILLS){
-                        Config.GLOBAL_LOGGER.warning("CountSql["+countSql+"] Take "+diff);
+                        Config.GLOBAL_LOGGER.warning("CountSql["+newCountSql+"] Take "+diff);
                     }else{
-                        Config.GLOBAL_LOGGER.info("CountSql["+countSql+"] Take "+diff);
+                        Config.GLOBAL_LOGGER.info("CountSql["+newCountSql+"] Take "+diff);
                     }
                     countRs.next();
                     count = countRs.getInt(1);
@@ -143,11 +154,14 @@ public class JdbcDataAccess implements DataAccess<Connection>,Initializable{
                 if (count == 0) {
                     return new PageResult<>(new ArrayList<>(), 0);
                 } else {
-                    try(PreparedStatement ps = connection.prepareStatement(sql)) {
+                    SqlListResult sqlListResult= SqlUtil.replaceNull(sql, Arrays.asList(params));
+                    String newSql=sqlListResult.getSql();
+                    List<Object> newParamList= sqlListResult.getParamList();
+                    try(PreparedStatement ps = connection.prepareStatement(newSql)) {
                         int len = 0;
-                        if (params != null && (len = params.length) > 0) {
-                            for (int i = 1; i <= params.length; i++) {
-                                ps.setObject(i, params[i - 1]);
+                        if (newParamList != null && (len = newParamList.size()) > 0) {
+                            for (int i = 1; i <= newParamList.size(); i++) {
+                                ps.setObject(i, newParamList.get(i - 1));
                             }
                         }
                         ps.setObject(len + 1, (page.getPageNum() - 1) * page.getPageSize());
@@ -157,9 +171,9 @@ public class JdbcDataAccess implements DataAccess<Connection>,Initializable{
                         long t2=System.currentTimeMillis();
                         long diff=t2-t1;
                         if(diff>WARNING_SQL_TIME_MILLS){
-                            Config.GLOBAL_LOGGER.warning("Sql["+sql+"] Take "+diff);
+                            Config.GLOBAL_LOGGER.warning("Sql["+newSql+"] Take "+diff);
                         }else{
-                            Config.GLOBAL_LOGGER.info("Sql["+sql+"] Take "+diff);
+                            Config.GLOBAL_LOGGER.info("Sql["+newSql+"] Take "+diff);
                         }
                         List<T> dataList = new ArrayList<>();
                         while (rs.next()) {
