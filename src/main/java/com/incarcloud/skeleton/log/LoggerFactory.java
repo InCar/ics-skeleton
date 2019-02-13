28470f63e25b886ec25fd9e591b16a9c16162d18
ThomasChant <xgct0505@qq.com>
2019-02-13 14:55:26 +0800

    log配置修改

--- src/main/java/com/incarcloud/skeleton/log/LoggerFactory.java
+++ src/main/java/com/incarcloud/skeleton/log/LoggerFactory.java
@@ -14,28 +14,29 @@ import java.util.logging.*;
 
 public class LoggerFactory {
     private static SimpleFormatter getSimpleFormatter(){
-        SimpleFormatter simpleFormatter=new SimpleFormatter(){
-            private DateTimeFormatter dateTimeFormatter= DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS").withZone(ZoneId.systemDefault());
-            @Override
-            public synchronized String format(LogRecord record) {
-                ////2018-12-12 17:06:17.279 [main] WARN  o.h.e.jdbc.env.internal.JdbcEnvironmentInitiator - HHH000342: Could not obtain connection to query metadata : Communications link failure
-                StringBuilder sb=new StringBuilder();
-                String dateStr=dateTimeFormatter.format(Instant.ofEpochMilli(record.getMillis()));
-                sb.append(dateStr);
-                sb.append(" [");
-                sb.append(record.getThreadID());
-                sb.append("] ");
-                sb.append(record.getLevel().getName());
-                sb.append("  ");
-                sb.append(record.getSourceClassName());
-                sb.append(".");
-                sb.append(record.getSourceMethodName());
-                sb.append(" - ");
-                sb.append(formatMessage(record));
-                sb.append("\n");
-                return sb.toString();
-            }
-        };
+        SimpleFormatter simpleFormatter = new SimpleFormatter() {
+                private DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS").withZone(ZoneId.systemDefault());
+
+                @Override
+                public synchronized String format(LogRecord record) {
+                    ////2018-12-12 17:06:17.279 [main] WARN  o.h.e.jdbc.env.internal.JdbcEnvironmentInitiator - HHH000342: Could not obtain connection to query metadata : Communications link failure
+                    StringBuilder sb = new StringBuilder();
+                    String dateStr = dateTimeFormatter.format(Instant.ofEpochMilli(record.getMillis()));
+                    sb.append(dateStr);
+                    sb.append(" [");
+                    sb.append(record.getThreadID());
+                    sb.append("] ");
+                    sb.append(record.getLevel().getName());
+                    sb.append("  ");
+                    sb.append(record.getSourceClassName());
+                    sb.append(".");
+                    sb.append(record.getSourceMethodName());
+                    sb.append(" - ");
+                    sb.append(formatMessage(record));
+                    sb.append("\n");
+                    return sb.toString();
+                }
+            };
         return simpleFormatter;
     }
 
@@ -64,13 +65,18 @@ public class LoggerFactory {
         if(logConfig.isEnableLog()) {
             SimpleFormatter simpleFormatter = getSimpleFormatter();
             if(logConfig.isEnableConsoleLog()){
-                logger.addHandler(getConsoleHandler(simpleFormatter));
+                ConsoleHandler consoleHandler = getConsoleHandler(simpleFormatter);
+                consoleHandler.setLevel(logConfig.getLevel());
+                logger.addHandler(consoleHandler);
             }
             if(logConfig.isEnableFileLog()) {
-                logger.addHandler(getFileHandler(simpleFormatter, logConfig.getFileLogDir()));
+                FileHandler fileHandler = getFileHandler(simpleFormatter, logConfig.getFileLogDir());
+                fileHandler.setLevel(logConfig.getLevel());
+                logger.addHandler(fileHandler);
             }
             logger.setLevel(logConfig.getLevel());
         }
         return logger;
     }
+
 }
